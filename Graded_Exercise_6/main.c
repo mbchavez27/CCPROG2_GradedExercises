3f8104bcf9579676dd2d78f5a4b6756f4c898b52
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-03-07 15:10:23 +0800

    fix: add spaces for bill content

--- Graded_Exercise_6/main.c
+++ Graded_Exercise_6/main.c
@@ -5,25 +5,30 @@
 #define MAX_BILLS 10
 
 typedef char String25[26];
+typedef char String50[51];
 
-typedef struct {
+typedef struct
+{
   String25 firstName;
   String25 lastName;
   String25 middleInitial;
 } Name;
 
-typedef struct {
+typedef struct
+{
   int month;
   int year;
   int day;
 } Date, Birthday;
 
-typedef struct {
-  String25 name;
+typedef struct
+{
+  String50 content;
   Date date;
 } Bill;
 
-typedef struct {
+typedef struct
+{
   float confidenceVotes;
   float possibleVotes;
   float percentage;
@@ -31,7 +36,8 @@ typedef struct {
   Date date;
 } Rating;
 
-typedef struct {
+typedef struct
+{
   Name name;
   String25 position;
   String25 party;
@@ -41,8 +47,10 @@ typedef struct {
   Rating rating;
 } Candidate;
 
-void convertMonthtoWord(int month) {
-  switch (month) {
+void convertMonthtoWord(int month)
+{
+  switch (month)
+  {
   case 1:
     printf("January");
     break;
@@ -82,7 +90,8 @@ void convertMonthtoWord(int month) {
   }
 }
 
-void getName(Name *name) {
+void getName(Name *name)
+{
   char nameInput[26];
   printf("Give Candidate First Name: ");
   scanf("%s", nameInput);
@@ -95,7 +104,8 @@ void getName(Name *name) {
   strcpy(name->lastName, nameInput);
 }
 
-void getDate(Date *date) {
+void getDate(Date *date)
+{
   int numDate;
 
   printf("Give Year: ");
@@ -109,18 +119,21 @@ void getDate(Date *date) {
   date->day = numDate;
 }
 
-void getBill(Bill *bill) {
-  String25 input;
+void getBill(Bill *bill)
+{
+  String50 input;
 
-  printf("Give Bill Name: ");
-  scanf("%s", input);
-  strcpy(bill->name, input);
+  printf("Give Bill Content: ");
+  scanf(" ");
+  scanf("%[^\n]", input);
+  strcpy(bill->content, input);
 
   printf("Give Bill Date:\n");
   getDate(&bill->date);
 }
 
-void getRating(Rating *rating) {
+void getRating(Rating *rating)
+{
   String25 input;
   int inputInt;
 
@@ -142,7 +155,8 @@ void getRating(Rating *rating) {
   getDate(&rating->date);
 }
 
-void getInput(Candidate *candidate, String25 parties[], int *numOfParties) {
+void getInput(Candidate *candidate, String25 parties[], int *numOfParties)
+{
   int i = 0;
   int stop = 0;
   String25 input;
@@ -153,12 +167,14 @@ void getInput(Candidate *candidate, String25 parties[], int *numOfParties) {
   printf("Give Candidate Birthday:\n");
   getDate(&candidate->birthday);
   printf("Give Position: ");
-  scanf("%s", input);
+  scanf(" ");
+  scanf("%[^\n]", input);
   strcpy(candidate->position, input);
   printf("Give Party: ");
   scanf("%s", input);
   strcpy(candidate->party, input);
-  while (i < MAX_BILLS && !stop) {
+  while (i < MAX_BILLS && !stop)
+  {
     char input[2];
     getBill(&candidate->billsPassed[candidate->numOfBillsPassed]);
     candidate->numOfBillsPassed++;
@@ -171,51 +187,62 @@ void getInput(Candidate *candidate, String25 parties[], int *numOfParties) {
   printf("\n\n");
 }
 
-void displayDate(Date date) {
+void displayDate(Date date)
+{
   convertMonthtoWord(date.month);
   printf(" %d, %d\n", date.day, date.year);
 }
 
-void displayBill(Bill bill[], int numOfBillsPassed) {
+void displayBill(Bill bill[], int numOfBillsPassed)
+{
   int i = 0;
-  for (i = 0; i < numOfBillsPassed; i++) {
-    printf("Bill Passed: %s\n", bill[i].name);
+  for (i = 0; i < numOfBillsPassed; i++)
+  {
+    printf("Bill Passed: %s\n", bill[i].content);
     printf("Date Passed: ");
     displayDate(bill[i].date);
   }
 }
 
-void swapCandidates(Candidate *candidate1, Candidate *candidate2) {
+void swapCandidates(Candidate *candidate1, Candidate *candidate2)
+{
   Candidate temp = *candidate1;
   *candidate1 = *candidate2;
   *candidate2 = temp;
 }
 
-void sortAlphabetical(Candidate candidate[], int numOfCandidates) {
+void sortAlphabetical(Candidate candidate[], int numOfCandidates)
+{
   int i = 0;
   int j = 0;
 
-  for (i = 0; i < numOfCandidates - 1; i++) {
-    for (j = i + 1; j < numOfCandidates; j++) {
+  for (i = 0; i < numOfCandidates - 1; i++)
+  {
+    for (j = i + 1; j < numOfCandidates; j++)
+    {
       if (strcmp(candidate[i].name.lastName, candidate[j].name.lastName) > 0)
         swapCandidates(&candidate[i], &candidate[j]);
     }
   }
 }
 
-void sortByRating(Candidate candidate[], int numOfCandidates) {
+void sortByRating(Candidate candidate[], int numOfCandidates)
+{
   int i = 0;
   int j = 0;
 
-  for (i = 0; i < numOfCandidates - 1; i++) {
-    for (j = i + 1; j < numOfCandidates; j++) {
+  for (i = 0; i < numOfCandidates - 1; i++)
+  {
+    for (j = i + 1; j < numOfCandidates; j++)
+    {
       if (candidate[i].rating.percentage < candidate[j].rating.percentage)
         swapCandidates(&candidate[i], &candidate[j]);
     }
   }
 }
 
-void display(Candidate candidate, int numOfCandidates) {
+void display(Candidate candidate, int numOfCandidates)
+{
   printf("Candidate: %d\n", numOfCandidates + 1);
   printf("Name: %s %s. %s\n", candidate.name.firstName,
          candidate.name.middleInitial, candidate.name.lastName);
@@ -235,42 +262,48 @@ void display(Candidate candidate, int numOfCandidates) {
   printf("\n\n");
 }
 
-void displaybyRating(Candidate candidate[], int numOfCandidates) {
+void displaybyRating(Candidate candidate[], int numOfCandidates)
+{
   int i = 0;
   sortByRating(candidate, numOfCandidates);
   for (i = 0; i < numOfCandidates; i++)
     display(candidate[i], i);
 }
 
-void displaybyParty(Candidate candidate[], char *party, int numOfCandidates) {
+void displaybyParty(Candidate candidate[], char *party, int numOfCandidates)
+{
   int i = 0;
-  int stop = 0;
-  while (i < numOfCandidates && !stop) {
-    char input[2];
-    if (strcmp(candidate[i].party, party) == 0) {
+  char input[10];
+  while (i < numOfCandidates)
+  {
+    if (strcmp(candidate[i].party, party) == 0)
+    {
       display(candidate[i], i);
+      printf("Type anything to continue...\n");
+      scanf("%9s", input); // Reads user input to proceed
     }
-    // printf("Next candidate: [any key]");
-    // scanf(" %c", input);
     i++;
   }
 }
 
-void displayAllCandidates(Candidate candidate[], int numOfCandidates) {
+void displayAllCandidates(Candidate candidate[], int numOfCandidates)
+{
   int i = 0;
   sortAlphabetical(candidate, numOfCandidates);
   for (i = 0; i < numOfCandidates; i++)
     display(candidate[i], i);
 }
 
-int main() {
+int main()
+{
   Candidate candidates[MAX_CANDIDATES];
   String25 parties[MAX_CANDIDATES];
   int numOfParties = 0;
   int numOfCandidates = 0;
   int input;
 
-  do {
+  do
+  {
     printf("Candidates Menu\n");
     printf("1. Add Candidate Info\n");
     printf("2. Display All candidates\n");
@@ -282,7 +315,8 @@ int main() {
     scanf("%d", &input);
     printf("\n\n");
 
-    switch (input) {
+    switch (input)
+    {
     case 1:
       getInput(&candidates[numOfCandidates], parties, &numOfParties);
       numOfCandidates++;
